900c2a0f01ea2b07e2279bac11a6895998c657ea
MohithMohan0708 <147494627+MohithMohan0708@users.noreply.github.com>
2025-03-17 19:40:40 +0530

    Time: 89 ms (9.04%) | Memory: 12.8 MB (1.06%) - LeetSync

---
+++ 2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def divideArray(self, nums):
+        count = 0 
+        n = len(nums)//2
+        d = {}
+        for i in range(len(nums)):
+            if nums[i] in d.keys():
+                d[nums[i]] = d[nums[i]] + 1
+            else:
+                d[nums[i]] = 1
+        values = d.values()
+        list_values = list(values)
+        for i in range(len(list_values)):
+            if list_values[i] % 2 == 0:
+                count += list_values[i]//2
+        return count == n
+
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        
