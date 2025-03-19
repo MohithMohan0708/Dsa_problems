6bb8a04efaf01294b2342b98516bd54444f810d1
MohithMohan0708 <147494627+MohithMohan0708@users.noreply.github.com>
2025-03-19 23:06:50 +0530

    Time: 4 ms (98.89%) | Memory: 45 MB (70.29%) - LeetSync

--- 2866-longest-even-odd-subarray-with-threshold/longest-even-odd-subarray-with-threshold.java
+++ 2866-longest-even-odd-subarray-with-threshold/longest-even-odd-subarray-with-threshold.java
@@ -1,29 +1,23 @@
 class Solution {
     public int longestAlternatingSubarray(int[] nums, int threshold) {
-        
-        int maxLength = 0;
-        int left = 0, right = 0;
-        
-        while (right < nums.length) {
-            if (nums[left] % 2 == 0 && nums[left] <= threshold) {
-                // Expand the window while maintaining alternation and threshold condition
-                while (right < nums.length && nums[right] <= threshold && validation(left, right, nums)) {
-                    maxLength = Math.max(right - left + 1, maxLength);
-                    right++;
-                }
-            } 
-            left++;
-            right = left; // Reset right pointer to start a new subarray
-        }
-        return maxLength;
-    }
-    
-    public boolean validation(int start, int end, int[] nums) {
-        for (int i = start; i < end; i++) {
-            if (nums[i] % 2 == nums[i + 1] % 2) {
-                return false;
+        int st = 0;
+        while(st<nums.length && (nums[st]%2!=0 || nums[st]>threshold))
+        st++;
+
+        int ws = st<nums.length?1:0;
+        for(int et = st+1;et<nums.length;et++){
+            if(nums[et]%2==nums[et-1]%2 || nums[et]>threshold)
+            {
+                st = et;
+                continue;
             }
+
+            while(st<nums.length && (nums[st]%2!=0 || nums[st]>threshold)){
+                st++;
+            }
+
+            ws = Math.max(ws, et-st+1);
         }
-        return true;
+        return ws;
     }
 }
