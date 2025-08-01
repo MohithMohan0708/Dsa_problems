fe7ab11b9c11867a77f8d7c93966d0ceef8d388a
MohithMohan0708 <147494627+MohithMohan0708@users.noreply.github.com>
2025-08-01 12:04:03 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (27.24%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -11,7 +11,7 @@ class Solution {
     public int rob(int[] nums) {
        int n = nums.length;
        if(n==1) return nums[0];
-       int[] dp = new int[n];
+       int[] dp = new int[n-1];
        Arrays.fill(dp,-1);
        int ans1 = func(n-2,nums,dp);
        int[] arr = new int[n-1];
