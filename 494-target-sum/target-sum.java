83daf930fa997685575e2509792b40e00df2ee83
MohithMohan0708 <147494627+MohithMohan0708@users.noreply.github.com>
2025-08-20 13:36:43 +0530

    Time: 4 ms (91.55%) | Memory: 43.4 MB (47.54%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,15 +1,30 @@
 class Solution {
-    public int findTargetSumWays(int[] nums, int target) {
-        return helper(nums, target, 0, 0);
+    public int countSubset(int i, int target, int[] arr,int[][] dp){
+        if(i==0){
+            if(target==0 && arr[0]==0) return 2;
+            if(target==arr[0] || target==0) return 1;
+            return 0;
+        }
+        if(dp[i][target]!=-1) return dp[i][target];
+        int notpick = countSubset(i-1,target,arr,dp);
+        int pick = 0;
+        if(target>=arr[i]){
+            pick = countSubset(i-1,target-arr[i],arr,dp);
+        }
+        return dp[i][target] = pick + notpick;
     }
-
-    private int helper(int[] arr, int target, int idx, int sum) {
-        if (idx >= arr.length)
-            return sum == target ? 1 : 0;
-        
-        int subtract = helper(arr, target, idx + 1, sum - arr[idx]);
-        int add = helper(arr, target, idx + 1, sum + arr[idx]);
-
-        return subtract + add;
+    public int findTargetSumWays(int[] nums, int target) {
+        int totalSum = 0;
+        int n = nums.length;
+        for(int i = 0; i < n; i++){
+            totalSum += nums[i];
+        }
+        if((totalSum-target) < 0 || (totalSum-target)%2!=0) return 0;
+        int k = (totalSum - target)/2;
+        int[][] dp = new int[n][k+1];
+        for(int[] row : dp){
+            Arrays.fill(row,-1);
+        }
+        return countSubset(n-1,k,nums,dp);
     }
 }
