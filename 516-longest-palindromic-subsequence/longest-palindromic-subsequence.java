e0b916233ca586451b958dc4a7ac25c9d6edf1a9
MohithMohan0708 <147494627+MohithMohan0708@users.noreply.github.com>
2025-08-22 19:26:15 +0530

    Time: 42 ms (62.27%) | Memory: 55.2 MB (59.81%) - LeetSync

--- 516-longest-palindromic-subsequence/longest-palindromic-subsequence.java
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.java
@@ -1,20 +1,20 @@
 class Solution {
-    public int lcs(int i,int j,String s,String sb,int[][] dp){
+    public int lcs(int i, int j, String s, String srev, int[][] dp){
         if(i<0 || j<0) return 0;
         if(dp[i][j]!=-1) return dp[i][j];
-        if(s.charAt(i)==sb.charAt(j)){
-            return dp[i][j] = 1 + lcs(i-1,j-1,s,sb,dp);
+        if(s.charAt(i)==srev.charAt(j)){
+            return dp[i][j] = 1 + lcs(i-1,j-1,s,srev,dp);
         }
-        dp[i][j] = Math.max(lcs(i-1,j,s,sb,dp),lcs(i,j-1,s,sb,dp));
-        return dp[i][j];
+        return dp[i][j] = Math.max(lcs(i-1,j,s,srev,dp),lcs(i,j-1,s,srev,dp));
     }
     public int longestPalindromeSubseq(String s) {
-        String sb = new StringBuilder(s).reverse().toString();
+        StringBuilder sreverse = new StringBuilder(s).reverse();
+        String srev = sreverse.toString();
         int n = s.length();
         int[][] dp = new int[n][n];
-        for(int[] row: dp){
+        for(int[] row : dp){
             Arrays.fill(row,-1);
         }
-        return lcs(n-1,n-1,s,sb,dp);
+        return lcs(n-1,n-1,s,srev,dp);
     }
 }
